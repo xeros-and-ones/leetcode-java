4b79ebddad7b4fd35b34b16b2f1dc5f97fd4b28d
Mohamed Tarek <mohamed96tarek@hotmail.com>
2025-06-07 15:33:50 +0300

    ....

--- src/remove_element/Solution.java
+++ src/remove_element/Solution.java
@@ -1,31 +1,38 @@
 package remove_element;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
-    /// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
-    ///  The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
-    /// Consider the number of elements in nums which are not equal to val be k,
-    ///  to get accepted, you need to do the following things:
-    /// Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
-    /// The remaining elements of nums are not important as well as the size of nums.
-    /// Return k.
+  /// Given an integer array nums and an integer val, remove all occurrences of val in nums
+  // in-place.
+  ///  The order of the elements may be changed. Then return the number of elements in nums which
+  // are not equal to val.
+  /// Consider the number of elements in nums which are not equal to val be k,
+  ///  to get accepted, you need to do the following things:
+  /// Change the array nums such that the first k elements of nums contain the elements which are
+  // not equal to val.
+  /// The remaining elements of nums are not important as well as the size of nums.
+  /// Return k.
 
-    public static int removeElement(int[] nums, int val) {
-        if (nums.length == 0) return 0;
+  public static int removeElement(int[] nums, int val) {
+    if (nums.length == 0) return 0;
+    List<Integer> tmp = new ArrayList<>();
 
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[j] != val || nums[i] ==val) {
-                i++;
-                nums[i] = nums[j];
-            }
-            System.out.println(nums[i]);
-        }
-        return i;
+    for (int num : nums) {
+      if (num != val) {
+        tmp.add(num);
+      }
+    }
+    for (int i = 0; i < tmp.size(); i++) {
+      nums[i] = tmp.get(i);
     }
 
-    public static void main(String[] args) {
+    return tmp.size();
+  }
 
-        System.out.println(removeElement(new int[]{3, 2, 2, 3}, 3));
-    }
+  public static void main(String[] args) {
 
+    System.out.println(removeElement(new int[] {3, 2, 2, 3}, 3));
+  }
 }
