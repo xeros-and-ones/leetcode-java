841f8de76e9101fe3665a3c2d5606272de5a0430
Mohamed Tarek <mohamed96tarek@hotmail.com>
2025-06-07 17:57:45 +0300

    Shuffle The Array Solution

--- src/shuffle_the_array/Solution.java
+++ src/shuffle_the_array/Solution.java
@@ -4,33 +4,36 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Solution {
-  public static int[] flattenMap(TreeMap<Integer, Integer> map) {
-    int[] res = new int[map.size() * 2];
-    int idx = 0;
-    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-      res[idx++] = entry.getKey();
-      res[idx++] = entry.getValue();
-    }
-    return res;
-  }
+    public static int[] shuffleLinearSpace(int[] nums, int n) {
+        int[] res = new int[nums.length];
 
-  public static int[] shuffle(int[] nums, int n) {
-
-    if (nums.length == 0) return null;
-
-    TreeMap<Integer, Integer> pairs = new TreeMap<>();
-
-    int y = n;
-    for (int i = 0; i < n; i++) {
-      pairs.put(nums[i], nums[y++]);
+        for (int i = 0; i < n; i++) {
+            res[2 * i] = nums[i];
+            res[2 * i + 1] = nums[i + n];
+        }
+        return res;
     }
 
-    flattenMap(pairs);
-    System.out.println(pairs);
-    return nums;
-  }
+    public static int[] shuffleConstantSpace(int[] nums, int n) {
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] << 10;
+            nums[i] = nums[i] | nums[i + n];
+        }
+        int j = 2 * n - 1;
 
-  public static void main(String[] args) {
-    shuffle(new int[] {2, 5, 1, 3, 4, 7}, 3);
-  }
+        for (int i = n - 1; i > -1; i--) {
+            int y = nums[i] & ((1 << 10) - 1);
+            int x = nums[i] >> 10;
+
+            nums[j] = y;
+            nums[j - 1] = x;
+            j -= 2;
+        }
+        for (int num : nums) System.out.println(num);
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        shuffleConstantSpace(new int[]{2, 5, 1, 3, 4, 7}, 3);
+    }
 }
